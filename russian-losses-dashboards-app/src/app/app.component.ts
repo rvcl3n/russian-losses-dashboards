d416d360c64ab422ef8222f7c8d474daf33d3e12
rvcl3n <d.rudenko.1996@gmail.com>
2022-09-01 02:19:33 +0300

    add chart

--- russian-losses-dashboards-app/src/app/app.component.ts
+++ russian-losses-dashboards-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as HighCharts from 'highcharts';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'russian-losses-dashboards-app';
+  
+  highcharts = HighCharts;
+
+  chartOptions: Highcharts.Options = {
+    title: {
+      text: "russian losses"
+    },
+    xAxis: {
+      categories: ["March","April","May","June","July","August"],
+      title: {
+        text: "Months"
+      }
+    },
+    yAxis: {
+      title: {
+        text: "Losses"
+      }
+    },
+    series: [
+      {
+      name: "Troops",
+      data: [3900,5800,7200,5050,5280,6870],
+      type: 'line'
+      }
+  ]
+  }
 }
--- russian-losses-dashboards-app/src/app/app.module.ts
+++ russian-losses-dashboards-app/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HighchartsChartModule } from 'highcharts-angular';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HighchartsChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
