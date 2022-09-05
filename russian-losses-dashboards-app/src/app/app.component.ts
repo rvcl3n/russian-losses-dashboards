6bf00bff428f260c2758b215d892d44f01a09116
rvcl3n <d.rudenko.1996@gmail.com>
2022-09-06 01:04:02 +0300

    add september

--- russian-losses-dashboards-app/src/app/app.component.ts
+++ russian-losses-dashboards-app/src/app/app.component.ts
@@ -20,19 +20,18 @@ export class AppComponent {
       type: 'bar', //this denotes tha type of chart
 
       data: {// values on X-Axis
-        labels: ["March","April","May","June","July","August"], 
+        labels: ["March","April","May","June","July","August","September(updating)"], 
 	       datasets: [
           {
             label: "Troops Losses",
-            data: [3900,5800,7200,5050,5280,6870],
+            data: [3900,5800,7200,5050,5280,6870,1450],
             backgroundColor: 'rgba(255, 99, 132, 0.8)',
             barThickness : 50
           }
         ]
       },
       options: {
-        aspectRatio: 3
-        
+        aspectRatio: 3        
       }     
     });
   }
