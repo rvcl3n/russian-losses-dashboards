c853d85cf200ca14d80b4524cb0c40dc90ffd991
rvcl3n <d.rudenko.1996@gmail.com>
2022-09-09 02:07:24 +0300

    add blank totallosses component

--- russian-losses-dashboards-app/src/app/app.module.ts
+++ russian-losses-dashboards-app/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { LayoutComponent } from './layout/layout.component';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { TotalLossesComponent } from './total-losses/total-losses.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.com
     LayoutComponent,
     HeaderComponent,
     SidenavListComponent,
+    TotalLossesComponent,
   ],
   imports: [
     BrowserModule,
---
+++ russian-losses-dashboards-app/src/app/total-losses/total-losses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-total-losses',
+  templateUrl: './total-losses.component.html',
+  styleUrls: ['./total-losses.component.css']
+})
+export class TotalLossesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
