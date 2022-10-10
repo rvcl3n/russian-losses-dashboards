8a56b31f3e28ba39ed2282e2aeebcf48ed836746
rvcl3n <d.rudenko.1996@gmail.com>
2022-10-11 01:48:03 +0300

    add angular tooltip

--- russian-losses-dashboards-app/src/app/material/material.module.ts
+++ russian-losses-dashboards-app/src/app/material/material.module.ts
@@ -5,6 +5,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   imports: [
@@ -13,14 +14,16 @@ import { MatListModule } from '@angular/material/list';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatListModule
+    MatListModule,
+    MatTooltipModule
   ],
   exports: [
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatListModule
+    MatListModule,
+    MatTooltipModule
   ],
   declarations: []
 })
