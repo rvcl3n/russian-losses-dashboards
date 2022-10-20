9caef888a0e8c9d6d6d996e3b87bd9dc4d97e63d
rvcl3n <d.rudenko.1996@gmail.com>
2022-10-21 01:18:20 +0300

    update formatting

--- russian-losses-dashboards-app/src/app/app.module.ts
+++ russian-losses-dashboards-app/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { LayoutComponent } from './layout/layout.component';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { TotalLossesComponent } from './total-losses/total-losses.component';
