76aba920a5e69bc54a84cf3a78ba9a62ec887b48
rvcl3n <d.rudenko.1996@gmail.com>
2022-10-21 01:38:48 +0300

    add chart-props helper

---
+++ russian-losses-dashboards-app/src/app/helpers/chart-props.ts
@@ -0,0 +1,41 @@
+export class ChartProps
+{
+    public static PersonnelBGColor: string = 'rgba(255, 99, 132, 0.8)';
+    public static PersonnelBorderColor: string = 'rgb(255, 0, 0)';
+    
+    public static TanksChartBGColor: string = 'rgba(50, 205, 50, 0.8)';
+    public static TanksChartBorderColor: string = 'rgb(34, 139, 34)';
+
+    public static AFVChartBGColor: string = 'rgba(255, 99, 71, 0.8)';
+    public static AFVChartBorderColor: string = 'rgb(255, 69, 0)';
+
+    public static ArtilleryChartBGColor: string = 'rgba(0, 255, 255, 0.8)';
+    public static ArtilleryChartBorderColor: string = 'rgb(0, 206, 209)';
+
+    public static MLRSChartBGColor: string = 'rgba(255, 255, 102, 0.8)';
+    public static MLRSChartBorderColor: string = 'rgb(215, 215, 0)';
+    
+    public static ADFChartBGColor: string = 'rgba(238, 130, 238, 0.8)';
+    public static ADFChartBorderColor: string = 'rgb(255, 0, 255)';
+
+    public static JetsChartBGColor: string = 'rgba(135, 206, 250, 0.8)';
+    public static JetsChartBorderColor: string = 'rgb(30, 144, 255)';
+    
+    public static HelicoptersChartBGColor: string = 'rgba(32, 178, 170, 0.8)';
+    public static HelicoptersChartBorderColor: string = 'rgb(85, 140, 100)';
+
+    public static UAVChartBGColor: string = 'rgba(255, 215, 0, 0.8)';
+    public static UAVChartBorderColor: string = 'rgb(218, 165, 32)';
+
+    public static CruiseMissilesChartBGColor: string = 'rgba(169, 169, 169, 0.8)';
+    public static CruiseMissilesChartBorderColor: string = 'rgb(128, 128, 128)';
+
+    public static WarShipsChartBGColor: string = 'rgba(23, 104, 238, 0.8)';
+    public static WarShipsChartBorderColor: string = 'rgb(106, 90, 205)';
+
+    public static VehiclesChartBGColor: string = 'rgba(165, 42, 42, 0.8)';
+    public static VehiclesChartBorderColor: string = 'rgb(128, 0, 0)';
+
+    public static SpecialEquipmentChartBGColor: string = 'rgba(255, 182, 193, 0.8)';
+    public static SpecialEquipmentChartBorderColor: string = 'rgb(255, 105, 180)';
+}
