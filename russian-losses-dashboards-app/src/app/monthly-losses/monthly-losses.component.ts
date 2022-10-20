0416a7699294e6d56915b62c86194a931f179121
rvcl3n <d.rudenko.1996@gmail.com>
2022-10-21 00:16:09 +0300

    add monthly losses tanks and afv

--- russian-losses-dashboards-app/src/app/monthly-losses/monthly-losses.component.ts
+++ russian-losses-dashboards-app/src/app/monthly-losses/monthly-losses.component.ts
@@ -16,19 +16,43 @@ export class MonthlyLossesComponent implements OnInit {
   }
 
   createChart(){
-    let map = new Map<string, number>();
+    let troopsMap = new Map<string, number>();
+    let tanksMap = new Map<string, number>();
+    let afvMap = new Map<string, number>();
 
     for (var key in data) {
 
-      if (map.get((data[key]['date'].split('-'))[1]))
+      if (troopsMap.get((data[key]['date'].split('-'))[1]))
       {
-        let val = map.get((data[key]['date'].split('-'))[1])!;
+        let val = troopsMap.get((data[key]['date'].split('-'))[1])!;
 
-        map.set((data[key]['date'].split('-'))[1], data[key]['personnelDetla'] + val);
+        troopsMap.set((data[key]['date'].split('-'))[1], data[key]['personnelDetla'] + val);
       }
       else
       {
-        map.set((data[key]['date'].split('-'))[1], data[key]['personnelDetla']);
+        troopsMap.set((data[key]['date'].split('-'))[1], data[key]['personnelDetla']);
+      }
+
+      if (tanksMap.get((data[key]['date'].split('-'))[1]))
+      {
+        let val = tanksMap.get((data[key]['date'].split('-'))[1])!;
+
+        tanksMap.set((data[key]['date'].split('-'))[1], data[key]['tanksDelta'] + val);
+      }
+      else
+      {
+        tanksMap.set((data[key]['date'].split('-'))[1], data[key]['tanksDelta']);
+      }
+
+      if (afvMap.get((data[key]['date'].split('-'))[1]))
+      {
+        let val = afvMap.get((data[key]['date'].split('-'))[1])!;
+
+        afvMap.set((data[key]['date'].split('-'))[1], data[key]['afvDelta'] + val);
+      }
+      else
+      {
+        afvMap.set((data[key]['date'].split('-'))[1], data[key]['afvDelta']);
       }
     }
 
@@ -36,12 +60,24 @@ export class MonthlyLossesComponent implements OnInit {
       type: 'bar', //this denotes tha type of chart
 
       data: {// values on X-Axis
-        labels: Array.from(map.keys()), 
+        labels: Array.from(troopsMap.keys()), 
 	       datasets: [
           {
             label: "Troops Losses",
-            data: Array.from(map.values()),
+            data: Array.from(troopsMap.values()),
             backgroundColor: 'rgba(255, 99, 132, 0.8)',
+            barThickness : 50
+          },
+          {
+            label: "Tanks Losses",
+            data: Array.from(tanksMap.values()),
+            backgroundColor: 'rgba(50, 205, 50, 0.8)',
+            barThickness : 50
+          },
+          {
+            label: "AFV Losses",
+            data: Array.from(afvMap.values()),
+            backgroundColor: 'rgba(255, 99, 71, 0.8)',
             barThickness : 50
           }
         ]
